07515f580d9429caff23e1ff15709a0d5b7ea3ea
xandfcosta <fcosta.xand@gmail.com>
2023-10-05 18:02:40 -0300

    feat project init

---
+++ src/main/java/com/BancoFoda/BancoFoda/BancoFodaApplication.java
@@ -0,0 +1,13 @@
+package com.BancoFoda.BancoFoda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoFodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoFodaApplication.class, args);
+	}
+
+}
