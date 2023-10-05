ff67e8f1db53f56359d9ef1bdace22694461f934
xandfcosta <fcosta.xand@gmail.com>
2023-10-05 18:03:34 -0300

    feat create Fatura

---
+++ src/main/java/com/BancoFoda/BancoFoda/banco/Fatura.java
@@ -0,0 +1,47 @@
+package com.BancoFoda.BancoFoda.banco;
+
+import java.util.Date;
+
+public class Fatura
+{
+    private float valor;
+    private Date vencimento;
+    private Date dataPagamento;
+
+    public Fatura( float valor, Date vencimento, Date dataPagamento )
+    {
+        this.valor = valor;
+        this.vencimento = vencimento;
+        this.dataPagamento = dataPagamento;
+    }
+
+    public float getValor( )
+    {
+        return valor;
+    }
+
+    public void setValor( float valor )
+    {
+        this.valor = valor;
+    }
+
+    public Date getVencimento( )
+    {
+        return vencimento;
+    }
+
+    public void setVencimento( Date vencimento )
+    {
+        this.vencimento = vencimento;
+    }
+
+    public Date getDataPagamento( )
+    {
+        return dataPagamento;
+    }
+
+    public void setDataPagamento( Date dataPagamento )
+    {
+        this.dataPagamento = dataPagamento;
+    }
+}
