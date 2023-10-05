7e1f99d9620846f6de2dfaf63b6b2e31ba363607
xandfcosta <fcosta.xand@gmail.com>
2023-10-05 18:03:27 -0300

    feat create Credito

---
+++ src/main/java/com/BancoFoda/BancoFoda/banco/Credito.java
@@ -0,0 +1,47 @@
+package com.BancoFoda.BancoFoda.banco;
+
+import java.util.ArrayList;
+
+public class Credito
+{
+    private float valorTotal;
+    private float valorAtual;
+    private ArrayList< Fatura > faturas;
+
+    public Credito( float valorTotal, float valorAtual, ArrayList< Fatura > faturas )
+    {
+        this.valorTotal = valorTotal;
+        this.valorAtual = valorAtual;
+        this.faturas = faturas;
+    }
+
+    public float getValorTotal( )
+    {
+        return valorTotal;
+    }
+
+    public void setValorTotal( float valorTotal )
+    {
+        this.valorTotal = valorTotal;
+    }
+
+    public float getValorAtual( )
+    {
+        return valorAtual;
+    }
+
+    public void setValorAtual( float valorAtual )
+    {
+        this.valorAtual = valorAtual;
+    }
+
+    public ArrayList< Fatura > getFaturas( )
+    {
+        return faturas;
+    }
+
+    public void setFaturas( ArrayList< Fatura > faturas )
+    {
+        this.faturas = faturas;
+    }
+}
