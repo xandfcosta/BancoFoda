fd24fab908db987350c75cd2c36316c4d7768895
xandfcosta <fcosta.xand@gmail.com>
2023-12-05 18:38:04 -0300

    fix merger error

--- src/main/java/com/BancoFoda/BancoFoda/model/repository/conta/FaturaRepository.java
+++ src/main/java/com/BancoFoda/BancoFoda/model/repository/conta/FaturaRepository.java
@@ -2,11 +2,13 @@ package com.BancoFoda.BancoFoda.model.repository.conta;
 
 import com.BancoFoda.BancoFoda.model.domain.conta.Fatura;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface FaturaRepository extends JpaRepository< Fatura, Long>
 {
-    Optional< List<Fatura> > findByDataVencimento( String data);
+    @Query(value = "SELECT * FROM fatura f WHERE f.data_vencimento = ?1", nativeQuery = true)
+    Optional< List<Fatura> > findByDataVencimento( String data );
 }
--- src/main/java/com/BancoFoda/BancoFoda/model/repository/movimentacao/PagamentoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.BancoFoda.BancoFoda.model.repository.movimentacao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PagamentoRepository extends JpaRepository< Pagamento, Integer >
-{
-}
